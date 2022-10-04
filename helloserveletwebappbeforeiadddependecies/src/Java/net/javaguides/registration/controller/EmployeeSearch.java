44bc96fcafa47005b02bd9888ef8ae5b2c857d37
smallchungus <wchen1396@gmail.com>
2022-10-04 14:10:03 -0400

    okay so I'm getting tomcat is not starting at localhost so i'm going to push this and then I'm going to check what would happen if i deleted the files.

--- helloserveletwebappbeforeiadddependecies/src/Java/net/javaguides/registration/controller/EmployeeSearch.java
+++ helloserveletwebappbeforeiadddependecies/src/Java/net/javaguides/registration/controller/EmployeeSearch.java
@@ -14,14 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class search
  */
-@WebServlet("/register")
-public class EmployeeServlet extends HttpServlet {
+@WebServlet("/search")
+public class EmployeeSearch extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public EmployeeServlet() {
+	public EmployeeSearch() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
@@ -42,11 +42,47 @@ public class EmployeeServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		try {
+			// if (request.getParameter("submit") != null) {
+
+			PrintWriter out = response.getWriter();
+			out.println("Welcome to Search HI");
+
+			String toBeSearched = request.getParameter("search");
+			out.println("To Be Searched: " + toBeSearched);
+			searchFunction ( toBeSearched ); 
+		} catch (Exception e) {
+			System.out.println("there is an error in the doPost method <br>");
+			e.printStackTrace();
+		}
 
 		doGet(request, response);
 	}
 	
 	private void searchFunction ( String toBeSearched )
 	{
+		try {
+			File myObj = new File("C:\\Users\\wchen\\eclipse\\jee-2022-06\\eclipse\\filenameNew.txt");
+			Scanner myReader = new Scanner(myObj);
+			
+			int count = 0; 
+			while (myReader.hasNextLine()) 
+			{
+				String data = myReader.nextLine().toLowerCase().toString();
+				if ( data.contains(toBeSearched))
+				{
+				System.out.println(data);
+				count++;
+				}
+			}
+			
+			System.out.println("Number of times your inquiry is in the file " + count);
+			myReader.close();
+		} catch (NullPointerException e) {
+			System.out.println("File not found here");
+		} catch (Exception e) {
+			System.out.println("exception occured");
+		}
 	}
-	}
+
+}
